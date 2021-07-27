4a361548c0d047439018a067f179cd6178ab2ff2
SilkaPraveesh <silkakukku92@gmail.com>
2021-07-27 20:55:53 +0400

    Initial Commit

---
+++ src/test/java/commyProject/myFirstDemoProject/MyFirstDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package commyProject.myFirstDemoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
